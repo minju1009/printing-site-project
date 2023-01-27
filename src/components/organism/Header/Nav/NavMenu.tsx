21ec78e78b00d1cde429b0bb0c84d752e343fa2d
Minju <minju1009@gmail.com>
2023-01-28 05:49:27 +0900

    OPM-12 Add : 헤더 드롭다운 생성 및 기존 Nav hover시 보여주기

--- src/components/organism/Header/Nav/NavMenu.tsx
+++ src/components/organism/Header/Nav/NavMenu.tsx
@@ -1,43 +1,60 @@
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import styled, { css } from 'styled-components';
 
 import { fontSize, theme } from 'styles';
 
+import DropdownMenu from './DropdownMenu';
+
 interface INavMenuProps {
   children: ReactNode;
   path: string;
   isNew?: boolean;
+  menuId: number;
 }
 
-export default function NavMenu({ children, path, isNew }: INavMenuProps) {
+export default function NavMenu({ children, path, isNew, menuId }: INavMenuProps) {
   const { pathname } = useLocation();
+  const [dropdownMenuId, setDropdownMenuId] = useState(0);
+
+  const handleMouseEnter = () => {
+    setDropdownMenuId(menuId);
+  };
+
+  const handleMouseLeave = () => {
+    setDropdownMenuId(0);
+  };
 
   return (
-    <Container>
+    <Container onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
       <Link to={path}>
         {isNew && (
           <NewIcon>
             <img alt="new icon" src="/images/ic-new.svg" />
           </NewIcon>
         )}
-        <Text selected={pathname === path}>{children}</Text>
+        <Text hover={dropdownMenuId === menuId} selected={pathname === path}>
+          {children}
+        </Text>
       </Link>
+      {dropdownMenuId === menuId && <DropdownMenu dropdownMenuId={dropdownMenuId} />}
     </Container>
   );
 }
 
 interface ISelectedStyle {
   selected: boolean;
+  hover: boolean;
 }
 
 const Container = styled.li`
   position: relative;
+  padding: 40px 0;
 `;
 
 const NewIcon = styled.div`
   position: absolute;
-  top: -25px;
+  top: 16px;
   left: 50%;
   transform: translateX(-50%);
 `;
@@ -52,7 +69,9 @@ const Text = styled.span<ISelectedStyle>`
       color: ${theme.BLUE_DARK};
     `}
 
-  :hover {
-    color: ${theme.BLUE_DARK};
-  }
+  ${({ hover }) =>
+    hover &&
+    css`
+      color: ${theme.BLUE_DARK};
+    `}
 `;
