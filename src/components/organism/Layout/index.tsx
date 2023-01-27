4b454b8dc308ad74f04aa5c95f01a1609488fcd9
Minju <minju1009@gmail.com>
2023-01-27 16:17:39 +0900

    OPM-9 레이아웃 추가

---
+++ src/components/organism/Layout/index.tsx
@@ -0,0 +1,29 @@
+import { ReactNode } from 'react';
+import styled from 'styled-components';
+
+import Header from 'components/organism/Header';
+import { flex } from 'styles';
+
+interface ILayoutProps {
+  children: ReactNode;
+}
+
+export default function Layout({ children }: ILayoutProps) {
+  return (
+    <Container>
+      <Header />
+      <Body>{children}</Body>
+      {/* <Footer /> */}
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  ${flex('', 'center', 'column')}
+`;
+
+const Body = styled.main`
+  width: 1140px;
+  height: 960px;
+  background-color: #5e5e43;
+`;
