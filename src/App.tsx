16e8ef677ad2672dba282e70ae8d037d3303600a
Minju <minju1009@gmail.com>
2023-01-29 11:19:05 +0900

    OPM-14 App.tsx에 RecoilRoot 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 import BusinessCardStore from 'pages/BusinessCardStore';
 import GlobalStyle from 'styles/globalStyles';
 
 function App() {
   return (
-    <>
+    <RecoilRoot>
       <GlobalStyle />
       <BrowserRouter>
         <Routes>
           <Route element={<BusinessCardStore />} path="/" />
         </Routes>
       </BrowserRouter>
-    </>
+    </RecoilRoot>
   );
 }
 
