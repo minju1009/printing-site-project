199b8de6a0cdb01be0bca674f46f6ba591d1a223
Minju <minju1009@gmail.com>
2023-01-29 22:39:31 +0900

    OPM-18 Fix typos

--- src/components/molecules/CheckboxFilter/index.tsx
+++ src/components/molecules/CheckboxFilter/index.tsx
@@ -1,7 +1,7 @@
 import Layout from './components/Layout';
 import List from './components/List';
 import Title from './components/Title';
-import Checkbox from 'components/atoms/Checkbox/Checkbox';
+import Checkbox from 'components/atoms/Checkbox/CheckboxWithTextLabel';
 
 const CheckboxFilter = Object.assign(Layout, {
   List,
